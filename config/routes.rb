16a088bb3e4d7d8aa24c88bd10f168c76414544f
Spencer Harrell <spencerharrell@Spencers-MBP.attlocal.net>
2016-01-27 13:05:09 -0500

    Added Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  
+
+  devise_for :users, controllers: { registrations: "registrations"}
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  root 'pages#home'
   # root 'welcome#index'
 
   # Example of regular route:
