08d7e24043fecebd2490553c0b63ffa7c12d3d88
Spencer Harrell <spencerharrell@Spencers-MBP.attlocal.net>
2016-02-01 00:24:20 -0500

    Added the average rating

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,13 +1,19 @@
 class Place < ActiveRecord::Base
-	validates_presence_of :name
-	validates_presence_of :phone
-	validates_presence_of :address
-	validates_presence_of :website
-	validates_presence_of :user_id
-	belongs_to :user
+	validates_presence_of :name 
+	validates_presence_of :phone 
+	validates_presence_of :address 
+	validates_presence_of :website 
+	validates_presence_of :user_id 
 
-	geocoded_by :address
-	after_validation :geocode
+	belongs_to :user
+	geocoded_by :address 
+	after_validation :geocode 
 
 	has_many :reviews, dependent: :destroy
+
+	def average_rating
+		self.reviews.sum(:score) / reviews.size 
+	rescue ZeroDivisionError
+		0
+	end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,16 @@
 class Review < ActiveRecord::Base
-belongs_to :user 
+	belongs_to :user 
 	belongs_to :place
 
 	validates_presence_of :content
 	validates_presence_of :score 
+
+	after_save :calculate_average
+
+	def calculate_average
+		a = self.place.average_rating
+		self.place.update_attributes(total_average: a)
+	end
+
 end
+
